5d256d8ee4deb0c6023850878aba3a038adfa675
zjie <928952347@qq.com>
2020-02-19 10:45:07 +0800

    这是一个测试Md文档

---
+++ teamwork/teamwork-dao/src/main/java/com/nf/dao/UserDao.java
@@ -0,0 +1,25 @@
+package com.nf.dao;
+
+/**
+ * @Author: LJP
+ * @Classname UserDao
+ * @Date: 2020-02-19 07:46
+ * @Description:
+ */
+public class UserDao {
+    public static void main(String[] args) {
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+        System.out.println("我是测试的UserDao。。。。。。。。。。。。。。");
+    }
+}
